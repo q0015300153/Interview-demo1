ff7bcb70993a635571b8e1ddca0d622c56469a99
TB <q0015300153@gmail.com>
2021-07-25 12:49:53 +0800

    done

--- add-laravel.bat
+++ add-laravel.bat
@@ -2,17 +2,6 @@
 FOR /F "tokens=1,2delims==" %%x IN (.env) DO (IF NOT "%%y"=="" (SET %%x=%%y))
 
 FOR /F "delims=: tokens=2" %%a IN ('exec.bat ls "/var/www/html/%LaravelName%"') DO (SET f="%%a")
-<<<<<<< HEAD
-IF NOT %f%=="" (
-	IF "%LaravelFrom%"=="" (
-		REM 建立專案
-		exec.bat composer create-project --prefer-dist laravel/laravel ${LaravelName}
-		exec.bat chmod -R 757 ${LaravelName}
-		exec.bat cd ${LaravelName} && chown -R \$USER:www-data storage
-		exec.bat cd ${LaravelName} && chown -R \$USER:www-data bootstrap/cache
-		exec.bat cd ${LaravelName} && chmod -R 775 storage
-		exec.bat cd ${LaravelName} && chmod -R 775 bootstrap/cache
-=======
 IF NOT "%f%"=="" (
 	IF "%LaravelFrom%"=="" (
 		REM 建立專案
@@ -22,7 +11,6 @@ IF NOT "%f%"=="" (
 		exec.bat "cd %LaravelName% && chown -R \$USER:www-data bootstrap/cache"
 		exec.bat "cd %LaravelName% && chmod -R 775 storage"
 		exec.bat "cd %LaravelName% && chmod -R 775 bootstrap/cache"
->>>>>>> addVersion
 	) ELSE (
 		REM 克隆專案
 		./exec.bat %LaravelFrom%
